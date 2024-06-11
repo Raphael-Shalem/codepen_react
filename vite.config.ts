abe7d18a18ac4cd8a64d3477940436e5bc596747
Raphael-Shalem <40669804+Raphael-Shalem@users.noreply.github.com>
2024-06-11 18:53:07 +0300

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: "https://www.yisumatica.org.il/competition5/",
+  plugins: [react(), tsconfigPaths() ],
+})
